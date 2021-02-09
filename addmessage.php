957e2198faa96b91a50456ab73febc1cab666e56
sewaca <53119694+sewaca@users.noreply.github.com>
2021-02-09 20:47:08 +0500

    Update addmessage.php

--- addmessage.php
+++ addmessage.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-<link rel="stylesheet" type="text/css" href="styles.css">
-<title>Передача показаний приборов учета в компанию Байрам</title>
+ <link rel="stylesheet" type="text/css" href="styles.css">
+ <title>Передача показаний приборов учета в компанию Байрам</title>
 </head>
+<script>
+var str = "";
+function showUser(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        if (window.XMLHttpRequest) {
+            // код для IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // код для IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","getinfo.php?q="+str,true);
+        xmlhttp.send();
+    }
+}
+document.getElementById('counter_number').value = str;
+</script>
 <body align="center" style="margin-top:30px;">
+<div id="min"></div>
 <?php
 $servername = "localhost";
 $username = "id15961621_user";
@@ -15,53 +41,80 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } 
 catch(PDOException $e) { 
-    echo "Не удалось подключиться к БД. Попробуйте повторить попытку позднее <br>";
+    echo "Не удалось подключиться к БД. Попробуйте повторить попытку позднее ";
     }
-$result_id = (int)$_POST['id_user'];
-$result_type = $_POST['counter_type'];
-$result_number = $_POST['counter_number'];
-$result_res = (int)$_POST['result_number'];
-$result_date = date_create('now')->format('Y-m-d H:i:s');
-$sqli = "SELECT * FROM result WHERE result_id = ? ORDER BY result_date DESC LIMIT 1";
-$stmt= $conn->prepare($sqli);
-$stmt->execute([$result_id]);
-//если таблица не пустая
-if (($stmt->rowCount()) > 0) {
-    //для каждого столбца таблицы result - я думаю нужно это будет удалить, у нас таблица result теперь из одной строки
-    foreach ($stmt as $row) {
-        if ($row['result_res']>=$result_res) {
-            echo "<h2>Внимание! Текущие показания меньше или равны <br>показаниям за предыдущие месяцы. <br>Повторите ввод корректных показаний<br></h2>";
-                echo "<p><input type='submit' onclick='history.back();' value='Ввести показания'/></p>";
-        break;
-        }
-        //если все показания в норме, то запишем их
-        else {
-            $sql = "INSERT INTO result (result_id, result_type, result_number, result_res, result_date) VALUES (?,?,?,?,?)";
-            $stmt= $conn->prepare($sql);
-            $stmt->execute([$result_id, $result_type, $result_number, $result_res, $result_date]);
-            echo "<h2>Показания внесены успешно, спасибо!<br></h2>";
-            break;
-        }
-    }
+$flag = false;
+$id_user = array();
+$stmt = $conn->prepare('SELECT * FROM users WHERE user_login = ? AND user_password = ?');
+$stmt->execute(array($_POST['user_login'], $_POST['user_password']));
+while ($row = $stmt->fetch(PDO::FETCH_LAZY)) {
+    $flag = true;
+    $id_user = $row['user_id'];
 }
-//если таблица пустая, то никаких проверок не нужно, забиваем сразу показания
-else {
-    $sql = "INSERT INTO result (result_id, result_type, result_number, result_res, result_date) VALUES (?,?,?,?,?)";
-    //проверка чтоб все показания были больше 0
-    if ($result_res>0) {
-        $stmt= $conn->prepare($sql);
-        $stmt->execute([$result_id, $result_type, $result_number, $result_res, $result_date]);
-        echo "<h2><b>Показания внесены успешно, спасибо!</b></h2>";
-    }
-    else{
-        echo "<h2><b>Вы не ввели никаких данных! <br>Вернитесь назад и повторите попытку </b><br></h2>";
-        echo "<p><input type='submit' onclick='history.back();' value='Ввести показания'/></p>";
-    }
+$_monthsList = array(
+"1"=>"январь","2"=>"февраль","3"=>"март",
+"4"=>"апрель","5"=>"май", "6"=>"июнь",
+"7"=>"июль","8"=>"август","9"=>"сентябрь",
+"10"=>"октябрь","11"=>"ноябрь","12"=>"декабрь");
+$month = $_monthsList[date("n")];
+?>
+<h2>Добро пожаловать в личный кабинет!</h2>
+<h2>Внесение показаний прибора учета для субарендаторов <br>за месяц <u><?php echo $month; ?></u></h2>
+<?php 
+$stmt = $conn->prepare('SELECT * FROM information WHERE info_id = ?');
+$stmt->execute(array($id_user));
+while ($row = $stmt->fetch(PDO::FETCH_LAZY)) {
+    $flag = true;
+    $info_name = $row['info_name'];
+    $info_inn = $row['info_inn'];
+    $info_adress = $row['info_adress'];
 }
 ?>
-<br>
-<input type="submit" onclick='history.back();' value='Назад'/>
-<form method="post" action="index.php">
-    <br><input type="submit" value="Выход" />
-</form>    
+<div>
+<p><b>Наименование юр.лица: </b><?php echo $info_name; ?><br></p>
+<p><b>ИНН: </b><?php echo $info_inn; ?><br></p>
+<p><b>Адрес объекта: </b><?php echo $info_adress; ?><br></p>
+</div>
+<?php if ($flag): 
+$sql = "SELECT * FROM counters WHERE counter_id = ?";
+$stmt= $conn->prepare($sql);
+$stmt->execute(array($id_user));
+?>
+<form method="get">
+<select name="users" onchange="showUser(this.value)">
+<option value="">Выберите номер счетчика:</option><br>
+<?php
+while($row = $stmt->fetch(PDO::FETCH_LAZY)){
+    $temp = $row['counter_number'];
+    $type = $row['counter_type'];
+?>
+    <option id="option" value="<?php echo $row['counter_number']?>"><?php echo $row['counter_number'] ?> </option> </p>
+<?php
+}
+?>
+</select>
+</form>
+<div id="txtHint"><b>Информация о предыдущих введенных показаниях будет указана тут...</b></div>
+<form name="test" method="POST" action="addmessage.php">
+
+  <input type="hidden" name="id_user" value="<?php echo $id_user; ?>"> 
+  <input type="hidden" name="counter_number" value="" id="counter_number">
+  <input type="hidden" name="counter_type" value="<?php echo $type; ?>" id="counter_type"> 
+  <p>Текущее показание:<br>
+   <input style="margin-top:5px;" name="result_number" type="text" size="40">
+  </p>
+  <p><input type="submit" value="Передать">
+   <input type="reset" value="Очистить"></p>
+ </form>
+ <script>
+     document.querySelector("body > form:nth-child(5) > select").addEventListener('input', ()=>{
+         document.querySelector("#counter_number").value = event.target.value;
+     })
+ </script>
+<?php 
+else: ?>
+<h2 align="center">Неправильный логин или пароль. Повторите попытку авторизации!</h2>
+<p><input type="submit" onclick="history.back();" value="Войти в личный кабинет"/></p>
+<?php endif ?>
 </body>
+</html>
